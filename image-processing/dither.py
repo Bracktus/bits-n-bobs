91fe2894baf379802ec4d870b98b0e83754888ba
Ricky <ricky@rchu.cc>
2021-08-14 17:18:06 +0100

    remove venv

--- image-processing/dither.py
+++ image-processing/dither.py
@@ -25,6 +25,7 @@ def transformImage(trans_mat, image, x, y):
         for j in range(dither_len):
             pixel = trans_mat[i][j]
             image[x + i, y + j] = pixel
+            print(f"x,y: ({x + i}, {y+j})")
 
 dither_matrix = [[0, 8, 2, 10 ],
                  [12, 4, 14, 6],
@@ -41,15 +42,16 @@ new_size = (width * dither_len, height * dither_len)
 new_image = Image.new('1', new_size)
 new_pixels = new_image.load()
 
+#TODO fix this bug:
+#   the dithering is overlapping. we need to offset it by 4 each loop
+
 new_x = 0
 new_y = 0
 for x in range(width):
     for y in range(height):
         old_pixel, a = old_pixels[x, y]
         trans_mat = applyDither(dither_matrix, old_pixel)
-        
-        new_x = x * 4
-        new_y = y * 4
-        transformImage(trans_mat, new_pixels, new_x, new_y)
+        transformImage(trans_mat, new_pixels, x * 4, y * 4)
 
 new_image.save("./images/dither.png")
+
