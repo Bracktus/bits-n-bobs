d5e8fdf2935de7487e946137ac1f5a5672a60b13
Ricky <ricky@rchu.cc>
2021-10-12 22:41:57 +0100

    poly progress

--- art/racket/poly.rkt
+++ art/racket/poly.rkt
@@ -1,63 +1,72 @@
 #lang racket
 
+(provide poly+)
+(provide poly*)
+(provide interpolate)
+
+(define points (list (cons 1 1) (cons 2 4) (cons 7 9)))
 ; a polynomial is a list of coefficients. '(2 0 3) -> 2 + 0x + 3x^2
 
 ;converts a polynomial into a function
-(define (poly->func poly))
+;(define (poly->func poly))
 
 ;adds 2 polynomials
-(define (poly+ poly-1 poly-2))
+(define (poly+ poly-1 poly-2)
+   (let loop ([p1 poly-1]
+              [p2 poly-2]
+              [acc '()])
+    (cond
+      [(and (null? p1) (null? p2)) (reverse acc)]
+      [(null? p1) (append acc p2)]
+      [(null? p2) (append acc p1)]
+      [else (loop (cdr p1)
+                  (cdr p2)
+                  (cons (+ (car p1) (car p2)) acc))])))
 
 ;multiply 2 polynomials
 (define (poly* poly-1 poly-2)
   
-  (let ([poly-len-1 (length poly-1)]
-        [poly-len-2 (length poly-2)]
-        [(list->vector lst)]
-        [(list->vector lst)])
-    
-    (define final
-      (make-vector (+ poly-len-1 poly-len-2) 0))
-  
-  (for* ([i (in-range poly-len-1)]
-         [j (in-range poly-len-1)])
+  (let* ([poly-len-1 (length poly-1)]
+         [poly-len-2 (length poly-2)]
+         [vec-1 (list->vector poly-1)]
+         [vec-2 (list->vector poly-2)]
+         [final (make-vector (sub1 (+ poly-len-1 poly-len-2)) 0)])
+        ;initalise a vector of length m + n - 1
+        ;where m = poly-len-1...
 
-    (vector-set! (+ i j) (* 
+    (for* ([i (in-range poly-len-1)]
+           [j (in-range poly-len-2)])
+      
+      (define curr-val (vector-ref final (+ i j)))
+      
+        (vector-set! final (+ i j) (+ curr-val (* (vector-ref vec-1 i)
+                                                  (vector-ref vec-2 j)))))
     
+    (vector->list final)))
+
 ;returns a n degree polynomial function passing through all the points
 (define (interpolate points)
   ;points is a list of xy pairs
-  (when (null? points)
-    (error "must provide at least 1 point")))
+  (define poly-list
+    (map (λ (point) (get-term point points)) points))
+
+  
+  (displayln poly-list)
+  (foldl poly+ '(0) poly-list))
 
 (define (get-term point points)
-  
-  (define (get-subterms x-1 x-2)
+
+  (define (get-subterms x1 x2)
     ;y * (x - xi / xi - xj)
     (let ([a0 (/ (- x2) (- x1 x2))]
           [a1 (/ 1 (- x1 x2))])
+      ;(displayln (list a0 a1))
       (list a0 a1)))
     
-    
-  (let* ([points (filter (λ (p) (= point p)) points)]
+  (let* ([n-points (remove point points)];ignore where xi = xj
          [x (car point)]
-         [subterms (map (λ (p) (get-subterms x (car p)) points))]) ;ignore where xi = xj
+         [y (cdr point)]
+         [subterms (map (λ (p) (get-subterms x (car p))) n-points)])
+    (poly* (list y) (foldl poly* '(1) subterms))))
 
-    (foldr poly* '(1) subterms)))
-  
-
-(define (draw)
-  (background 0)
-  ;update the dots
-  (hash-for-each dot-hash (λ (key val) (update-dot key)))
-  ;update sticks
-  (for ([stick stick-list])
-    (update-stick stick))
-  ;constrain the points
-  (hash-for-each dot-hash (λ (key val) (constrain-dots key)))
-  ;render sticks
-  (for ([stick stick-list])
-    (render-stick stick))
-  ;render the dots
-  (hash-for-each dot-hash (λ (key val) (render-dot val 5))))
 
