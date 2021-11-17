82e9dea50c50841a227ce4e308db0f845a2225ba
Bracktus <specialistricky@gmail.com>
2021-11-17 18:41:55 +0000

    clifford progress

--- art/racket/clifford.rkt
+++ art/racket/clifford.rkt
@@ -1,37 +1,55 @@
 #lang sketching
 
-(define a 2)
-(define b 2)
-(define c 1)
-(define d -1)
+(define a -1.3)
+(define b 1)
+(define c 1.7)
+(define d 1.7)
+(define r 130)
+(define iters 500000)
 
 (define (setup)
-  (size 600 600)
-  (background 0))
+  (size 1000 1000))
 
 (define (draw)
-
   (translate (/ width 2)
              (/ height 2))
   
-  (draw-clifford a b c d 0 0.1 200000)
+  (draw-clifford a b c d 0 0 iters)
+  (save "pics/blue.png")
   (no-loop))
+ 
+(define turq (color 9 243 247 1))
+(define dark-blue (color 31 79 183 1))
+(define purp (color 84 34 247 1))
+(define pink (color 247 34 183 1))
+(define orange (color 244 99 14 1))
+(define yellow (color 252 229 83 1))
+(define black (color 30 30 30))
+(define off-white (color 247 213 210))
 
 (define (draw-clifford a b c d init-x init-y count)
-  
   (no-fill)
-  (define colour (color 238 225 240 10))
-  (stroke colour)
+  (background black)
+  
   (let loop ([x init-x]
              [y init-y]
              [count count])
-
+    
     (define xy-pair
       (clifford a b c d x y))
 
-    (cond [(= count 0) (println "done!")]
+    (define dist
+      (sqrt (+ (sq (+ (car xy-pair) (cdr xy-pair)))
+               (sq (+ x y)))))
+    
+    (define lerp-val
+      (remap dist 0 4 0 1))
 
-          [else (point (* 100 x) (* 100 y))
+    (stroke (lerp-color turq dark-blue lerp-val))
+    
+    (cond [(= count 0)]
+
+          [else (point (* r x) (* r y))
                 (loop (car xy-pair)
                       (cdr xy-pair)
                       (sub1 count))])))
@@ -41,6 +59,6 @@
         [x2 (* c (cos (* a x)))]
         [y1 (sin (* b x))]
         [y2 (* d (cos (* b y)))])
-
+    
     (cons (+ x1 x2)
           (+ y1 y2))))
