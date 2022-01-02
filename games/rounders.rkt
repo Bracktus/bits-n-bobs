bb6b4e64802523ed0018d6ee00f8d1156841c768
Ricky <ricky@rchu.cc>
2022-01-02 12:38:19 +0000

    added intuitive grappling

--- games/rounders.rkt
+++ games/rounders.rkt
@@ -9,10 +9,6 @@
   ;either -1 or 1
   (define-values (direction) (values 1))
 
-  ;distance is the distance from our player and the ball
-  ;saving it to avoid recomputing it every frame.
-  (define-values (distance) (values 0))
-
   ;saving the previous values
   (define-values (trail-len) (values 30))
   (define-values (trail-x trail-y) (values (make-vector trail-len)
@@ -23,7 +19,7 @@
     (stroke 255)
     (render-trail)
     (unless (equal? 'empty attached)
-      (test-direction)
+      ;(test-direction)
       (render-attachment)))
   
   (define/public (update!)
@@ -43,9 +39,9 @@
     (when (or (> y height) (< y 0))
       (set! y-vel (- y-vel)))
 
-    ;when attached, set velocity to the perpendicular vector from player to ball
+    ;when attached, set velocity to the tangent vector of the player and the ball
     (unless (equal? attached 'empty)
-      (let-values ([(neo-x-vel neo-y-vel) (perp 8)])
+      (let-values ([(neo-x-vel neo-y-vel) (perp 10)])
         (set! x-vel (* direction neo-x-vel))
         (set! y-vel (* direction neo-y-vel))
         
@@ -63,45 +59,16 @@
       (vector-set! trail-y trail-idx y)))
 
   (define (get-direction)
-    (let* ([vel-norm-denom (sqrt (+ (sq x-vel) (sq y-vel)))]
-           [vel-norm-x (/ x-vel vel-norm-denom)]
-           [vel-norm-y (/ y-vel vel-norm-denom)]
-           
-           [a1 (/ pi 2)]
-           [x2 (+ x (* x-vel 300))]
-           [y2 (+ x (* y-vel 300))]
-           
-           [n-x (+ (attached.get-x) (* (/ (attached.get-r) 2) (cos a1)))]
-           [n-y (+ (attached.get-y) (* (/ (attached.get-r) 2) (sin a1)))]
-
-           [t (/ (+ (* (- n-x x) (- x2 x))
-                    (* (- n-y y) (- y2 y)))
-                 (sqrt (+ (sq (- x2 x)) (sq (- y2 y)))))])
-      (println t)
-      (if (or (< t 0) (> t 1)) -1 1)))
-
-  (define/public (test-direction)
-    (let* ([vel-norm-denom (sqrt (+ (sq x-vel) (sq y-vel)))]
-           [vel-norm-x (/ x-vel vel-norm-denom)]
-           [vel-norm-y (/ y-vel vel-norm-denom)]
-           
-           [x2 (+ x (* x-vel (/ (attached.get-r) 2)))]
-           [y2 (+ y (* y-vel (/ (attached.get-r) 2)))]
-           
-           [a1 (/ pi 2)]
-           [n-x (+ (attached.get-x) (* (/ (attached.get-r) 2) (cos a1)))] 
-           [n-y (+ (attached.get-y) (* (/ (attached.get-r) 2) (sin a1)))]
-
-           [t (/ (+ (* (- n-x x) (- x2 x))
-                    (* (- n-y y) (- y2 y)))
-                 (+ (sq (- x2 x)) (sq (- y2 y))))])
-      
-      (line x y x2 y2)))
-      ;(line x2 y2 n-x n-y)))
-                 
+    ;Decides if we should be rotating clockwise or anti-clockwise
+    ;https://stackoverflow.com/questions/13221873/determining-if-one-2d-vector-is-to-the-right-or-left-of-another
+    (let*  ([v1-x (- (attached.get-x) x)]
+            [v1-y (- (attached.get-y) y)]
+            [det (- (* v1-x y-vel)
+                    (* v1-y x-vel))])
+     (if (> det 0) -1 1)))
+        
   (define/public (attach! point-lst)
     (set! attached (nearest point-lst))
-    (set! distance (dist x y (attached.get-x) (attached.get-y)))
     (set! direction (get-direction)))
   
   (define/public (detach!)
@@ -121,21 +88,26 @@
       (circle (trail-x.ref idx)
               (trail-y.ref idx)
               (/ i 2))))
-      
+  
   (define (perp mult)
+    ;returns the tangent vector of Player and Ball
     (let* ([x-diff (- x (attached.get-x))]
            [y-diff (- y (attached.get-y))]
-           [x-norm (/ x-diff distance)]
-           [y-norm (/ y-diff distance)]
+
+           [d (dist x y (attached.get-x) (attached.get-y))]
+           ;Have to recompute distance every frame because adding perp vector is inprecise and slowly increases radius.
+           ;Saving it on attach produces a cool effect where the ball speeds up quickly though.
+           [x-norm (/ x-diff d)]
+           [y-norm (/ y-diff d)]
            [x (* mult x-norm)]
            [y (* mult y-norm)])
-    
-    (values (- y) x)))
+
+     (values (- y) x)))
 
   (define (nearest point-lst)
     (argmin (λ (p) (dist x y (p.get-x) (p.get-y)))
             point-lst))
-            
+        
   (define/public (render-attachment)
     (let* ([att-x (attached.get-x)]
            [att-y (attached.get-y)]
@@ -161,10 +133,16 @@
   (make-object Player 300 300 5 3 'empty))
 
 (define p1
-  (make-object Ball 300 300 20))
+  (make-object Ball 230 300 20))
+(define p2
+  (make-object Ball 200 200 20))
+(define p3
+  (make-object Ball 350 350 20))
+(define p4
+  (make-object Ball 230 350 20))
 
 (define p-lst
-  (list p1))
+  (list p1 p2 p3 p4))
 
 (define (setup)
   (size 600 600)
@@ -175,7 +153,8 @@
   (background 0)
   (me.update!)
   (me.render)
-  (p1.render))
+  (for ([p p-lst])
+    (p.render)))
 
 (define (on-key-pressed)
   (me.attach! p-lst))
